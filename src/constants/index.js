bc2d04784c0e4ee5c22dc9d7fbdd743f64a232ce
brgarciarivas <brgarciarivas@gmail.com>
2017-07-17 19:52:23 -0400

    set up basic push

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,7 +4,7 @@ export const API_VERSION = 1;
 //const API_ROOT_PROD = '';
 
 //local computer ip
-const API_ROOT_DEV = 'http://10.1.10.173:3000';
+const API_ROOT_DEV = 'http://192.168.10.59:3000';
 
 
 //export const API_ROOT = __DEV__ ? API_ROOT_DEV : API_ROOT_PROD;
